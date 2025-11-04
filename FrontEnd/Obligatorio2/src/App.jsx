b93ff8e1ea6faf22da1da1cf755308049b8064f2
Santiago-ORT <SG327471@fi365.ort.edu.uy>
2025-11-03 22:52:30 -0300

    primer commit front

--- FrontEnd/Obligatorio2/src/App.jsx
+++ FrontEnd/Obligatorio2/src/App.jsx
@@ -3,11 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes, Navigate} from "react-router-dom"
+import Login from '../Componentes/Login'
+import Home from '../Componentes/Home'
+import Carrito from '../Componentes/Carrito'
 function App() {
 
   return (
     <div>
+        <Routes>
+          <Route path='/' element={<Login/>}/>
+          <Route path='/home' element={<Home/>}/>
+          <Route path='/carrito' element={<Carrito/>}/>
 
+        </Routes>
     </div>
   )
 }
